c162baac863770ebbd27c7b02f729d20fddc2216
SoftwareSmeden <torb815g@edu.easj.dk>
2021-09-07 12:07:30 +0200

    Update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,8 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         // Think Java - Exercise 2.3
-        int hour = 16;
-        int minute = 27;
+        int hour = 14;
+        int minute = 55;
         int second = 35;
         double secondDeci = 35;
         int aFullDay = 24;
@@ -18,8 +18,18 @@ public class Main {
         System.out.print(":");
         System.out.println(second);
 
-        System.out.print("Number of minutes since midnight: ");
-        System.out.println((hour * 60) + minute + (secondDeci / 60));
+        String ascii =
+                ".'`~~~~~~~~~~~`'.\n" +
+                "(  .'11 12 1'.  )\n" +
+                "|  :10 \\    2:  |\n" +
+                "|  :9   @-> 3:  |\n" +
+                "|  :8       4;  |\n" +
+                "'. '..7 6 5..' .'\n" +
+                " ~-------------~ ";
+
+        System.out.println(ascii);
+
+        System.out.printf("Number of minutes since midnight: %.2f \n", (hour * 60) + minute + (secondDeci / 60));
 
         System.out.print("Number of seconds since midnight: ");
         System.out.println((hour * 3600) + (minute * 60) + second);
@@ -27,7 +37,7 @@ public class Main {
         System.out.print("Number of seconds remaining of the day: ");
         System.out.println((aFullDay * 3600) - ((hour * 3600) + (minute * 60) + second));
 
-        System.out.print("Percentage of the day that has passed: ");
-        System.out.println(((hour * 3600) + (minute * 60) + second) / (dayPercentage * 3600) * 100 + "%");
+        System.out.printf("Percentage of the day that has passed: %.4f%%", ((hour * 3600) + (minute * 60) + second) / (dayPercentage * 3600) * 100);
+
     }
 }
